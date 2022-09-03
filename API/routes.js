56299c11e2c62972b6faaf769475169dbd88c229
IanMGR <ianmarco.exe@gmail.com>
2022-09-03 07:46:02 -0300

    Deleting reference classes

--- API/routes.js
+++ API/routes.js
@@ -1,8 +1,6 @@
 const app = require("express");
 
 const userController = require("./src/controller/userController");
-const horarioController = require("./src/controller/horarioController");
-const porcaoUnicaController = require("./src/controller/porcaoUnicaController");
 const routes = app.Router();
 
 routes.get('/user',userController.index);
@@ -10,17 +8,5 @@ routes.get('/user',userController.index);
 routes.get('/user/login',userController.login);
 routes.get('/user/logout',userController.logout);
 routes.get('/user/callback',userController.callback);
-
-routes.get('/horario',horarioController.index);
-routes.post('/horario/add',horarioController.add);
-routes.get('/horario/all',horarioController.all);
-routes.get('/horario/toDispense',horarioController.toDispense);
-routes.get('/horario/dispensed/:id',horarioController.dispensedStatus);
-routes.get('/horario/:id',horarioController.read);
-routes.get('/horario/:id/delete',horarioController.delete);
-routes.post('/horario/update/:id',horarioController.update);
-
-routes.get('/porcaoUnica',porcaoUnicaController.read);
-routes.get('/porcaoUnica/:param',porcaoUnicaController.update);
 
 module.exports = routes;
---
+++ API/src/model/track.js
@@ -0,0 +1,9 @@
+class Track {
+  constructor(track) {
+    this.id = track.id;
+    this.name = track.name;
+    this.artist = track.artist;
+    this.album = track.album;
+    this.timestamp = track.timestamp;
+  }
+}
